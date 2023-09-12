132c4ea968bbd0a1e220114f18a15ac675145b30
Camh-git <cameronhillier1@gmail.com>
2023-09-12 11:40:54 +0100

    exercise
    The tasks we did live on the exercise
    Setup the react router
    created mainlayout
    imported router into and tidied up app
    added navlinks to header

--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -1,2 +1,21 @@
-export {}
-// what goes here??
+import { Routes, Route } from "react-router-dom";
+import Home from "../home/home";
+import Products from "../products/products";
+import About from "../about/about";
+import Help from "../help/help";
+import NotFound from "../not_found/not_found";
+import MainLayout from "../layouts/main_layouts";
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />}></Route>;
+        <Route path="/products" element={<Products />}></Route>;
+        <Route path="/about" element={<About />}></Route>;
+        <Route path="/help" element={<Help />}></Route>;
+        <Route path="*" element={<NotFound />}></Route>;
+      </Route>
+    </Routes>
+  );
+};
