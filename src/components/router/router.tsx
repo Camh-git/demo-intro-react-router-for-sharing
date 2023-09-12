a2726f23c119dea71ee5ed5d54c58cb49a4bbc7f
Camh-git <cameronhillier1@gmail.com>
2023-09-12 14:40:35 +0100

    product routes
    added specific product route to the router.
    added nav links for each product to the products page

--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../home/home";
 import Products from "../products/products";
+import Product from "../products/product/product";
 import About from "../about/about";
 import Help from "../help/help";
 import NotFound from "../not_found/not_found";
@@ -12,6 +13,7 @@ export const Router = () => {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />}></Route>;
         <Route path="/products" element={<Products />}></Route>;
+        <Route path="products:productId" element={<Product />}></Route>
         <Route path="/about" element={<About />}></Route>;
         <Route path="/help" element={<Help />}></Route>;
         <Route path="*" element={<NotFound />}></Route>;
